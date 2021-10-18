7ba6edbe0c4bda39122596d065aa09acb552e797
Marlcocoro <joongyue@gmail.com>
2021-10-18 13:12:15 +0900

    first commit

---
+++ src/main/java/com/group15/CreamCloneBackend/CreamCloneBackendApplication.java
@@ -0,0 +1,13 @@
+package com.group15.CreamCloneBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreamCloneBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreamCloneBackendApplication.class, args);
+	}
+
+}
