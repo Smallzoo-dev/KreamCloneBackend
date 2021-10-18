32f6811dca871844d971fbe03e17ee89a2fd4d7a
9sanha <64877575+9sanha@users.noreply.github.com>
2021-10-18 21:08:33 +0900

    user api 추가

---
+++ src/main/java/com/group15/CreamCloneBackend/domain/user/ResponseMsg.java
@@ -0,0 +1,18 @@
+package com.group15.CreamCloneBackend.domain.user;
+
+public enum ResponseMsg {
+
+    MSG_SUCCESS_SIGNUP("회원가입 성공"),
+    MSG_FAILE_SIGNUP("회원가입 실패"),
+    MSG_FAILE_LOGIN_USERNAME("로그인 실패 : 해당하는 사용자가 없습니다"),
+    MSG_FAILE_LOGIN_PASSWORD("로그인 실패 : 비밀번호를 확인해주세요"),
+    MSG_SUCCESS_LOGIN("로그인 성공"),
+    MSG_IS_USER("회원입니다"),
+    MSG_IS_ANONYMOUS("회원입니다");
+
+    private String msg;
+
+    ResponseMsg(String msg){
+        this.msg = msg;
+    }
+}
